4bb40273af988966f28e55b3feba08a514a22ca3
Maique Rosa <maique.rosa@4ward.com.br>
2019-08-11 21:05:32 -0300

    fix(ajuste nos imports e dependencias dos modulos): foi adicionado imports no modulo do component
    Import das dependencias do primeng nos modulo fourward-form e ajusts do package.json para adicionar
    as dependencias obrigatorias das libs

--- projects/fourward-utils/src/lib/fourward-form/fourward-form.module.ts
+++ projects/fourward-utils/src/lib/fourward-form/fourward-form.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { FourwardFormComponent } from './fourward-form.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FourwardInputMaskModule } from '../fourward-input-mask/fourward-input-mask.module';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FourwardInputMaskModule,
+    AutoCompleteModule,
+    CalendarModule
+  ],
   exports: [FourwardFormComponent],
   declarations: [FourwardFormComponent],
   providers: []
