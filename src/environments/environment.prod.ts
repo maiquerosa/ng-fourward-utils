eeb66d1652c4a042fafbce45a732feda87e59d93
Maique Rosa <maique.rosa@4ward.com.br>
2019-08-11 22:52:56 -0300

    fix: try upload the first package npm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  GITHUB_TOKEN: '941c1bb98bc918dd8019091bea0f0ab60030fcb8',
-  NPM_TOKEN: '57705451-24a1-4150-815c-852e76ee7a17'
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  GITHUB_TOKEN: '941c1bb98bc918dd8019091bea0f0ab60030fcb8',
-  NPM_TOKEN: '57705451-24a1-4150-815c-852e76ee7a17'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
